fa12783ff687850d3dafe9c9accdb1e71bb63c5a
r00tMG <meissagningue7@gmail.com>
2024-08-31 01:08:36 +0100

    change button to link

--- resources/views/ingredients/ingredients.blade.php
+++ resources/views/ingredients/ingredients.blade.php
@@ -19,7 +19,8 @@
           <div class="d-flex justify-content-between align-items my-3">
               <h6 class="card-title">Data Table Ingredients</h6>
               <a href="{{route('ingredients.create')}}" class="btn btn-primary">Create</a>
-          </div>        <div class="table-responsive">
+          </div>
+          <div class="table-responsive">
             @if(session('success'))
                 <div class="w-50 m-auto alert bg-alert-success">
                     <p class="text-center">{{ session('success') }}</p>
