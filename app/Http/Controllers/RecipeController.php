98ed51e167aa1f7896fe98ffdb05854b5a7626ca
Meissa Gningue <99593751+r00tMG@users.noreply.github.com>
2024-09-02 10:47:40 +0100

    Update RecipeController.php

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -13,31 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class RecipeController extends Controller
 {
-        foreach ($recipes as $recipe) {
-            $detailsResponse = Http::get('https://api.spoonacular.com/recipes/' . $recipe['id'] . '/information', [
-                'apiKey' => env('SPOONACULAR_API_KEY'),
-            ]);
-            $details = $detailsResponse->json();
-            //dd($details);
-
-            Recipe::updateOrCreate(
-                ['title' => $recipe['title']],
-                [
-                    'title' => $recipe['title'],
-                    'image' => $details['image'],
-                    'summary' => $details['summary'] ?? null,
-                    'ingredients' => json_encode(array_column($details['extendedIngredients'], 'original')),
-                ]
-            );
-        }
-        //dd($recipes);
-
-        return response()->json([
-            'error' => false,
-            'message' => "Vos recettes ont été généré avec succés",
-            'recipes' => $recipes
-        ], Response::HTTP_OK);
-    }
+       
     
 
     /**
